b7913abe6585680338d1fab0d03239587aaac4db
falco <gyrovorbis@gmail.com>
2023-02-03 12:21:42 -0500

    Resolved conflict with main's pvrmark.c.
    - Had to manually apply the fix from earlier -- lowering the start test
      count by shaving off a 0 to match main.

--- examples/dreamcast/pvr/pvrmark/pvrmark.c
+++ examples/dreamcast/pvr/pvrmark/pvrmark.c
@@ -173,7 +173,7 @@ int main(int argc, char **argv) {
     setup();
 
     /* Start off with something obscene */
-    switch_tests(2000000 / 60);
+    switch_tests(20000 / 60);
     begin = time(NULL);
 
     for(;;) {
